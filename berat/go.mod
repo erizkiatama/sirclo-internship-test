fdf924bcf922f33c4aadced2ee0d64189bd7a55a
erizkiatama <erizkiatama@gmail.com>
2020-11-07 21:05:32 +0800

    Added main application and environment variable

--- berat/go.mod
+++ berat/go.mod
@@ -3,7 +3,17 @@ module github.com/erizkiatama/berat
 go 1.13
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20191124224453-732737034ffd // indirect
+	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.0.1 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.1.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd // indirect
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
 )
---
+++ berat/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+
+	"github.com/erizkiatama/berat/controllers"
+	"github.com/erizkiatama/berat/models"
+
+	"github.com/joho/godotenv"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func initDB(dbHost, dbPort, dbUser, dbName, dbPassword string) *gorm.DB {
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUser, dbName, dbPassword)
+	db, err := gorm.Open("postgres", DBURL)
+	if err != nil {
+		fmt.Println("Cannot connect to the database")
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connected to the database")
+	}
+
+	db.AutoMigrate(&models.Weight{})
+
+	return db
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env: %s", err.Error())
+	}
+
+	db := initDB(os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
+
+	weightRepo := models.WeightRepository{DB: db}
+	weightController := controllers.WeightController{WeightRepo: weightRepo}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", weightController.Index).Methods("GET")
+	router.HandleFunc("/weight/{id}", weightController.Detail).Methods("GET")
+
+	fmt.Println("Listening to port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
